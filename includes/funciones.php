1fc561d2d11de9f85a3aca9109534361a6bebeb0
joalvarado2 <jonathanalva92@gmail.com>
2021-02-02 21:10:03 -0500

    finalizando la consulta de la db

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,18 +1,33 @@
 <?php
 
-function obtenerServicios() {
+function obtenerServicios() : array{
     try {
         // importar conexion de la base de datos (db)
-        require "database.php";
+        require 'includes/database.php';
 
+        $db->set_charset("utf8");
         // escribir codigo SQL
         $sql = "SELECT * FROM servicios;";
         $consulta = mysqli_query($db, $sql);
 
+        // arreglo vacio
+        $servicios = [];
+
+        $i = 0;
+
         // obtener los resultados
-        echo "<pre>";
-        var_dump(mysqli_fetch_assoc($consulta));
-        echo "<pre>";
+       while( $row = mysqli_fetch_assoc($consulta)) {
+           $servicios[$i]["id"] = $row["id"];
+           $servicios[$i]["nombre"] = $row["nombre"];
+           $servicios[$i]["precio"] = $row["precio"];
+
+           $i++;
+    }
+        return $servicios;
+
+      /*  echo "<pre>";
+       var_dump(json_encode($servicios));
+       echo "</pre>"; */
 
     } catch (\Throwable $th) {
         //throw $th;
